3a266eb379d48517e9b04c2712939ff0aff2ca0f
Michael Thalmeier <michael@thalmeier.at>
2018-01-10 19:54:31 +0100

    add yiimp_loop script
    yiimp_loop.py script calculates the most profitable coin every 10 minutes and
    outputs only the BTC profit in a single line.

---
+++ yiimp_loop.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python2
+from __future__ import print_function
+import requests
+import sys
+import time
+import hashrate_db
+import pools
+
+currency = 'EUR'
+power_cost = 0.15
+
+if len(sys.argv) < 2 or not sys.argv[1].lower() in hashrate_db.cards():
+    print('Need to give exactly one card: ' + ', '.join(hashrate_db.cards()))
+    exit(0)
+card = sys.argv[1]
+
+pool = 'bsod.pw'
+if len(sys.argv) >= 3:
+    pool = sys.argv[2]
+    if pool not in pools.YiimpPool.pool_urls.keys():
+        print('Pool not found. Available pools: ' + ','.join(pools.YiimpPool.pools_urls.keys()))
+        exit(0)
+
+p = pools.YiimpPool(pool, currency, power_cost)
+res = p.calc_profits(card, fetch=True)
+
+headers = ['time']
+headers.extend(res.keys())
+
+row_format_headers = '{:<20}' + '{:>14}' * len(res.keys())
+row_format = '{:<20}' + '{:>14.8f}' * len(res.keys())
+print (row_format_headers.format(*headers))
+
+while True:
+    fields = []
+    fields.append(time.strftime('%Y-%m-%d %H:%M:%S'))
+    for coin in res.keys():
+        fields.append(res[coin])
+    print(row_format.format(*fields))
+    sys.stdout.flush()
+    time.sleep(600)
+    res = p.calc_profits(card, fetch=True)
