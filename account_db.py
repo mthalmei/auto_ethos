b11d300d5b7eb6ed2c62c027b255dce805fece25
Michael Thalmeier <michael@thalmeier.at>
2018-02-21 09:29:29 +0100

    account_db: add PBS account info

--- account_db.py
+++ account_db.py
@@ -3,4 +3,5 @@ accounts = {
         'SPK': {'user': 'GJKyoWc5GpN4DTX2pB4S2hqKaghbj1riAj', 'pass': 'c=SPK'},
         'ORE': {'user': 'GYEzNVxe4p9aw8j5tzcwM1ex6UF68AbWsY', 'pass': 'c=ORE'},
         'MUN': {'user': 'M9n4xA1VcRbfvifggnNvZWkkDR17ezrPw8', 'pass': 'c=MUN'},
+        'PBS': {'user': 'DCKCZ8nMrEjcRqkNRUphQG83uHMeQPq6t1', 'pass': 'c=PBS'},
 }
